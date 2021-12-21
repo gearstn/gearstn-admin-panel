b1f9b802f8df910f308e791f8c7c6ffb6601f97e
petermalak <36422923+petermalak@users.noreply.github.com>
2021-12-21 14:40:58 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::group(['scheme' => 'https'], function () {
+Route::group(['scheme' => 'http'], function () {
 
 //Admin routes
 Route::prefix('admin')->group(function () {
