5c32e199892e74ab845197a129c9c0a71756a964
petermalak <pmalak14@gmail>
2021-12-23 14:03:40 +0200

    update database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Machine;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Contracts\Role;
 
 class DatabaseSeeder extends Seeder
 {
