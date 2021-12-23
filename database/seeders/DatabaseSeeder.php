1ad020ea9170e3d04b97d8e8b4d2e5571179eda6
petermalak <pmalak14@gmail>
2021-12-23 14:03:30 +0200

    edit database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-    }
+        Role::create(['name' => 'seller','guard_name' => 'api']);
+        Role::create(['name' => 'buyer','guard_name' => 'api']);
+
+        if (User::all()->count() == 0) $this->call(UserSeeder::class);
+        $this->call(UploadSeeder::class);
+        $this->call(CategorySeeder::class);
+        $this->call(SubCategorySeeder::class);
+        $this->call(ManufactureSeeder::class);
+        $this->call(CitySeeder::class);
+        $this->call(MachineModelSeeder::class);
+        // MachineModel::factory()->count(50)->create();
+        Machine::factory()->count(100)->create();    }
 }
