69182cb5c2c0d5f0d3c72465d42f7ce5c71df4a6
petermalak <pmalak14@gmail>
2022-01-12 13:41:13 +0200

    editing news

--- resources/views/admin/components/news/fields.blade.php
+++ resources/views/admin/components/news/fields.blade.php
@@ -32,10 +32,18 @@
     </div>
 </div>
 <div class="row">
-    <div class="col-sm-12">
+    <div class="col-sm-6">
         <div class="form-group">
-            {{ form::label('bodytext','Body Text')}}
-            {{form::textarea('bodytext',$news->bodytext,['class'=>'ckeditor form-control','style'=>'width: 100%'])}}
+            {{ form::label('bodytext','Body Text EN')}}
+            {{form::textarea('bodytext_en',$news->bodytext_en,['class'=>'ckeditor form-control','style'=>'width: 100%'])}}
+            {{-- <textarea class="ckeditor form-control" value = "{{ $news->bodytext }}" name="bodytext"
+                      style="width: 100%"></textarea> --}}
+        </div>
+    </div>
+    <div class="col-sm-6">
+        <div class="form-group">
+            {{ form::label('bodytext','Body Text AR')}}
+            {{form::textarea('bodytext_ar',$news->bodytext_ar,['class'=>'ckeditor form-control','style'=>'width: 100%'])}}
             {{-- <textarea class="ckeditor form-control" value = "{{ $news->bodytext }}" name="bodytext"
                       style="width: 100%"></textarea> --}}
         </div>
