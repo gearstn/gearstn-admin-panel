6422e40f553eb48d7484be968571cb555fa92240
petermalak <pmalak14@gmail>
2022-01-24 16:06:06 +0200

    adding news author , mins_read , seo title and description

--- resources/views/admin/components/news/fields.blade.php
+++ resources/views/admin/components/news/fields.blade.php
@@ -7,7 +7,7 @@
     </div>
     <div class="col-sm-4">
         <div class="form-group">
-            {{ form::label('title','English Title')}}
+            {{ form::label('title','English Arabic')}}
             {{form::text('title_ar', $news->title_ar ,['class'=>'form-control','placeholder'=>'Arabic Title'])}}
         </div>
     </div>
@@ -25,20 +25,43 @@
 </div>
 
 <div class="row">
+    <div class="col-sm-3">
+        <div class="form-group">
+            {{ form::label('author','Author')}}
+            {{form::text('author', $news->author ,['class'=>'form-control','placeholder'=>'Author'])}}
+        </div>
+    </div>
+    <div class="col-sm-3">
+        <div class="form-group">
+            {{ form::label('mins_read','Mins Read')}}
+            {{form::text('mins_read', $news->mins_read ,['class'=>'form-control','placeholder'=>'Mins Read'])}}
+        </div>
+    </div>
+    <div class="col-sm-3">
+        <div class="form-group">
+            {{ form::label('seo_title','SEO Title')}}
+            {{form::text('seo_title', $news->seo_title ,['class'=>'form-control','placeholder'=>'SEO Title'])}}
+        </div>
+    </div>
+    <div class="col-sm-3">
+        <div class="form-group">
+            {{ form::label('seo_description','SEO Description')}}
+            {{form::text('seo_description', $news->seo_description ,['class'=>'form-control','placeholder'=>'SEO Description'])}}
+        </div>
+    </div>
+</div>
+
+<div class="row">
     <div class="col-sm-6">
         <div class="form-group">
             {{ form::label('bodytext','Body Text EN')}}
             {{form::textarea('bodytext_en',$news->bodytext_en,['class'=>'ckeditor form-control','style'=>'width: 100%'])}}
-            {{-- <textarea class="ckeditor form-control" value = "{{ $news->bodytext }}" name="bodytext"
-                      style="width: 100%"></textarea> --}}
         </div>
     </div>
     <div class="col-sm-6">
         <div class="form-group">
             {{ form::label('bodytext','Body Text AR')}}
             {{form::textarea('bodytext_ar',$news->bodytext_ar,['class'=>'ckeditor form-control','style'=>'width: 100%'])}}
-            {{-- <textarea class="ckeditor form-control" value = "{{ $news->bodytext }}" name="bodytext"
-                      style="width: 100%"></textarea> --}}
         </div>
     </div>
 </div>
