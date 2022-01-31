6438b2abafda9b2d7c2170e7d4bde0a087251014
petermalak <pmalak14@gmail>
2022-01-31 16:32:04 +0200

    editing

--- resources/views/admin/components/news/fields.blade.php
+++ resources/views/admin/components/news/fields.blade.php
@@ -83,3 +83,13 @@
         </div>
     </div>
 </div>
+
+<script src="https://cdn.ckeditor.com/4.13.1/standard/ckeditor.js"></script>
+ 
+<script>
+    // 'X-CSRF-TOKEN': "{{ csrf_token() }}"
+    CKEDITOR.replace( 'bodytext_en', {
+        filebrowserUploadUrl: "{{route('uploads.local_storage')}}",
+        filebrowserUploadMethod: 'form'
+    });
+</script>
--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -49,7 +49,8 @@
 <script src="/styles/admin/dist/js/pages/dashboard.js"></script>
 <script src="//cdn.datatables.net/1.11.1/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js" defer></script>
-<script src="//cdn.ckeditor.com/4.16.2/standard/ckeditor.js"></script>
+{{-- <script src="//cdn.ckeditor.com/4.16.2/standard/ckeditor.js"></script> --}}
+<script src="https://cdn.ckeditor.com/4.13.1/standard/ckeditor.js"></script>
 
 @stack("scripts")
 </body>
