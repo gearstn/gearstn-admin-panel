d3a2e1c5c405255d58987711e712b66dce7b4e52
petermalak <pmalak14@gmail>
2022-02-15 16:39:24 +0200

    adding mail template job

---
+++ app/Jobs/MarketingMailJob.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\MarketingMail;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class MarketingMailJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $details;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($details)
+    {
+        $this->details = $details;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        foreach ($this->details['receiver'] as $email)
+            Mail::to($email)->send(new MarketingMail($this->details));
+    }
+}
