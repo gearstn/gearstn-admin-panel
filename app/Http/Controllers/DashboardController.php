30b3fc7d3cc53474e31d7cbd25cd4823d90d2380
kareemmoheb <kmoheb92@gmail.com>
2022-07-03 11:58:16 +0200

    adding spare parts to dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Modules\Machine\Entities\Machine;
+use Modules\SparePart\Entities\SparePart;
 
 class DashboardController extends Controller
 {
@@ -18,6 +19,9 @@ class DashboardController extends Controller
         $all_machines = Machine::all()->count() ;
         $today_machines = Machine::whereDate('created_at', Carbon::today())->count();
 
+        $all_spareparts = SparePart::all()->count() ;
+        $today_spareparts = SparePart::whereDate('created_at', Carbon::today())->count();
+
         $total_distributor = User::query()->whereHas("roles", function($q){ $q->where("name", ["distributor"]); })->count();
         $today_distributor = User::query()->whereHas("roles", function($q){ $q->where("name", ["distributor"]); })->whereDate('created_at', Carbon::today())->count();
 
@@ -25,14 +29,20 @@ class DashboardController extends Controller
         $today_contractor = User::query()->whereHas("roles", function($q){ $q->where("name", ["contractor"]); })->whereDate('created_at', Carbon::today())->count();
 
         $response = [
-            'users' => [['title' => 'all_users', 'count' => $all_users],
-            ['title' => 'today_users', 'count' => $today_users]],
-            'machines' => [['title' => 'all_machines', 'count' => $all_machines],
-            ['title' => 'today_machines', 'count' => $today_machines]],
-            'distributor' => [['title' => 'total_distributor', 'count' => $total_distributor],
-            ['title' => 'today_distributor', 'count' => $today_distributor]],
-            'contractor' => [['title' => 'total_contractor', 'count' => $total_contractor],
-            ['title' => 'today_contractor', 'count' => $today_contractor]],
+            'users' => [['title' => 'all users', 'count' => $all_users],
+            ['title' => 'Registered Today', 'count' => $today_users]],
+            
+            'machines' => [['title' => 'all machines', 'count' => $all_machines],
+            ['title' => 'Listed Today', 'count' => $today_machines]],
+            
+            'spare_parts' => [['title' => 'all spare parts', 'count' => $all_spareparts],
+            ['title' => 'Listed Today', 'count' => $today_spareparts]],
+
+            'distributor' => [['title' => 'total distributor', 'count' => $total_distributor],
+            ['title' => 'Registered Today', 'count' => $today_distributor]],
+
+            'contractor' => [['title' => 'total contractor', 'count' => $total_contractor],
+            ['title' => 'Registered Today', 'count' => $today_contractor]],
         ];
         return response()->json($response,200);
     }
